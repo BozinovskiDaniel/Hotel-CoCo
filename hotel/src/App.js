342c82a24eb56aac5b637a13df669430f383dffe
Daniel Bozinovski <BozinovskiDaniel@hotmail.com>
2019-09-05 09:17:57 +1000

    All Setup

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import { BrowsterRouter as Router, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="title">Hey Is this working</h1>
     </div>
   );
 }
